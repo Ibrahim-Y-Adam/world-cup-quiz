cf3943049535195169aae86c17a71978e46d94c5
Ibrahim-Y-Adam <ibroos@hotmail.com>
2023-09-20 02:15:08 +0100

    Modified background image and add some more style

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Who won the 2018 FIFA World Cup?",
+        answers: [
+            { text: "United Kingdom", correct: false},
+            { text: "Brazil", correct: false},
+            { text: "France", correct: true},
+            { text: "Spainm", correct: false},
+        ]   
+    }
+]
